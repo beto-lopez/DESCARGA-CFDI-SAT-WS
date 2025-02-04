8b82b6de92cd7f9ee871d77bccfeb24543e3cdeb
beto-lopez <162245106+beto-lopez@users.noreply.github.com>
2025-02-04 01:00:05 -0600

    Delete dmt directory

--- dmt/solicitude/batch/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Alberto Carlos Lopez Montemayor
- * All rights reserved.
- */
-/**
- * 
- * Documentation and javadocs in progress
- */
-package com.sicomsa.dmt.solicitude.batch;
