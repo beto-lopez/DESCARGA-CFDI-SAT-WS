0ad9f3918dc519266c01b235761c38acff209f65
albertoLopez <bananacharro@gmail.com>
2025-02-07 23:04:17 -0600

    abc1

--- src/main/java/com/sicomsa/dmt/Authorization.java
+++ src/main/java/com/sicomsa/dmt/Authorization.java
@@ -11,7 +11,8 @@ import java.time.Instant;
  *
  * @author https://www.linkedin.com/in/alberto-carlos-lopez-montemayor-586202198
  * 
- * @since 2024.9.9
+ * @version     2024.9.9
+ * @since       1.0
  * 
  * Contains the token provided from SAT to access massive download web services.
  * Includes instant the authorization was received, created and its expiration.
@@ -20,17 +21,30 @@ import java.time.Instant;
 public class Authorization implements java.io.Serializable { 
     private static final long serialVersionUID = 20241118L;
     
+    /**
+     * instant when message was received
+     */
     protected Instant instant;
+    /**
+     * instant when token was created
+     */
     protected Instant created;
+    /**
+     * instant when token will expire
+     */
     protected Instant expires;
+    /**
+     * token as it was received
+     */
     protected String token;
   
     /**
+     * builds an authorization with the received parameters
      * 
-     * @param satInstant
-     * @param created
-     * @param expires
-     * @param token 
+     * @param satInstant instant message was received
+     * @param created instant token was created
+     * @param expires intant token exprires
+     * @param token  received token
      * @throws IllegalArgumentException if any argument is null or if
      *          token is blank or expires instant is not after created instant
      */
@@ -50,30 +64,60 @@ public class Authorization implements java.io.Serializable {
         this.token = token;
     }
     
+    /**
+     * @param token
+     * @return true if token is not null and not blank
+     */
     public static boolean isConsistent(String token) {
         return (token != null && !token.isBlank());
     }
     
+    /**
+     * 
+     * @param created instant created
+     * @param expires instant expires
+     * @return  true if created and expires are not null and expires is after created
+     */
     public static boolean isConsistent(Instant created, Instant expires) {
         return (created != null && expires != null && expires.isAfter(created));
     }
     
+    /**
+     * 
+     * @return instant message was received
+     */
     public Instant getInstant() {
         return instant;
     }
     
+    /**
+     * 
+     * @return instant token was created
+     */
     public Instant getCreated() {
         return created;
     }
     
+    /**
+     * 
+     * @return instant token will expire
+     */
     public Instant getExpires() {
         return expires;
     }
     
+    /**
+     * 
+     * @return token received
+     */
     public String getToken() {
         return token;
     }
   
+    /**
+     * 
+     * @return string representation of <code>Authorization</code>
+     */
     @Override public String toString() {
         return new StringBuilder("Authorization:{")
                 .append("satInstant=").append(instant)
@@ -84,6 +128,11 @@ public class Authorization implements java.io.Serializable {
                 .toString();
     }
     
+    /**
+     * 
+     * @param token to be wrapped
+     * @return the token wrapped as needed to be sent in requests
+     */
     public static String wrapp(String token) {
         return new StringBuilder("WRAP access_token=\"")
             .append(token)
